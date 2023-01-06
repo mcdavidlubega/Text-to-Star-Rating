f21f967b8cdc1edee4a8c7ef3563a05ad525403a
McDavid Lubega <xenicmark@gmail.com>
2023-01-06 17:40:05 +0300

    refactored code for efficiency

--- stars.js
+++ stars.js
@@ -19,13 +19,13 @@ for (let i = 0; i < ratings.length; i++) {
   const newRatingsContainer = document.createElement('div');
   newRatingsContainer.className = 'ratingsContainer';
 
-  //add full stars to ratings container
-  for (let i = 0; i < fullStars; i++) {
+  function generateStars(percentageFill) {
     const newstarContainer = document.createElement('div');
     newstarContainer.className = 'starContainer';
 
     const starFill = document.createElement('div');
     starFill.className = 'starFill';
+    starFill.style.width = `${percentageFill}%`;
     const starFillImage = document.createElement('img');
     starFillImage.src = 'images/star.svg';
 
@@ -44,62 +44,20 @@ for (let i = 0; i < ratings.length; i++) {
       .appendChild(newRatingsContainer)
       .appendChild(newstarContainer);
   }
+  //add full stars to ratings container
+  for (let i = 0; i < fullStars; i++) {
+    generateStars(100);
+  }
 
   //add fractional star to ratings container if there are any
   if (fractionalStars > 0) {
-    const newstarContainer = document.createElement('div');
-    newstarContainer.className = 'starContainer';
-
-    const starFill = document.createElement('div');
-    starFill.className = 'starFill';
-    starFill.style.width = `${fractionalStars}%`;
-    const starFillImage = document.createElement('img');
-    starFillImage.src = 'images/star.svg';
-
-    const starOutline = document.createElement('div');
-    starOutline.className = 'starOutline';
-    const starOutlineImage = document.createElement('img');
-    starOutlineImage.src = 'images/outline.svg';
-
-    const newstarContainerFill = newstarContainer
-      .appendChild(starFill)
-      .appendChild(starFillImage);
-    const newstarContainerOutline = newstarContainer
-      .appendChild(starOutline)
-      .appendChild(starOutlineImage);
-    document
-      .getElementById('container')
-      .appendChild(newRatingsContainer)
-      .appendChild(newstarContainer);
+    generateStars(fractionalStars);
   }
 
   //add Empty Stars
 
   for (let i = 0; i < emptyStars; i++) {
-    const newstarContainer = document.createElement('div');
-    newstarContainer.className = 'starContainer';
-
-    const starFill = document.createElement('div');
-    starFill.className = 'starFill';
-    starFill.style.width = 0;
-    const starFillImage = document.createElement('img');
-    starFillImage.src = 'images/star.svg';
-
-    const starOutline = document.createElement('div');
-    starOutline.className = 'starOutline';
-    const starOutlineImage = document.createElement('img');
-    starOutlineImage.src = 'images/outline.svg';
-
-    const newstarContainerFill = newstarContainer
-      .appendChild(starFill)
-      .appendChild(starFillImage);
-    const newstarContainerOutline = newstarContainer
-      .appendChild(starOutline)
-      .appendChild(starOutlineImage);
-    document
-      .getElementById('container')
-      .appendChild(newRatingsContainer)
-      .appendChild(newstarContainer);
+    generateStars(0);
   }
 }
 
