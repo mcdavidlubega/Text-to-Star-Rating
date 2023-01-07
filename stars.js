f70be0b57ea9a9fe4686c595b17533d1cd8dd708
McDavid Lubega <xenicmark@gmail.com>
2023-01-07 16:41:34 +0300

    swapped image elemeents for svg elements

--- stars.js
+++ stars.js
@@ -1,3 +1,25 @@
+//create svg elements
+function makeStar(starType) {
+  const svgns = 'http://www.w3.org/2000/svg';
+  const svg = document.createElementNS(svgns, 'svg');
+  svg.setAttribute('viewBox', '0 0 24 22.94');
+  const path = document.createElementNS(svgns, 'path');
+  if (starType === 'filled') {
+    path.setAttribute(
+      'd',
+      'M12,19.21l5.82,3.52c1.07,.65,2.37-.31,2.09-1.51l-1.54-6.62,5.15-4.46c.94-.81,.43-2.36-.8-2.45l-6.77-.57L13.29,.85c-.48-1.14-2.1-1.14-2.58,0l-2.65,6.24-6.77,.57c-1.23,.1-1.74,1.64-.8,2.45l5.15,4.46-1.54,6.62c-.28,1.21,1.02,2.16,2.09,1.51l5.82-3.51Z'
+    );
+  } else {
+    path.setAttribute(
+      'd',
+      'M12,1c.1,0,.28,.03,.37,.24l2.65,6.25,.24,.56,.6,.05,6.78,.58c.23,.02,.31,.18,.34,.27,.03,.1,.06,.28-.12,.43l-5.15,4.46-.46,.4,.14,.59,1.54,6.62c.03,.13,0,.24-.07,.34-.08,.1-.2,.16-.32,.16-.07,0-.14-.02-.21-.06l-5.82-3.52-.52-.31-.52,.31-5.82,3.51c-.07,.04-.14,.06-.21,.06-.12,0-.24-.06-.32-.16-.08-.1-.1-.21-.07-.33l1.54-6.62,.14-.59-.46-.4L1.14,9.37c-.18-.15-.15-.33-.12-.43,.03-.09,.12-.25,.35-.27l6.77-.57,.6-.05,.24-.55L11.63,1.24c.09-.21,.27-.24,.37-.24m0-1c-.53,0-1.05,.28-1.29,.85l-2.65,6.24-6.77,.57c-1.23,.1-1.74,1.64-.8,2.45l5.15,4.46-1.54,6.62c-.22,.94,.52,1.72,1.36,1.72,.24,0,.49-.06,.73-.21l5.82-3.51,5.82,3.52c.24,.14,.49,.21,.73,.21,.84,0,1.58-.78,1.36-1.72l-1.54-6.62,5.15-4.46c.94-.81,.43-2.36-.8-2.45l-6.77-.57L13.29,.85c-.24-.57-.76-.85-1.29-.85h0Z'
+    );
+  }
+
+  svg.appendChild(path);
+  return svg;
+}
+
 //get all the ratings on the page so we cn iterate through them
 const ratings = document.querySelectorAll('.rating');
 
@@ -10,16 +32,13 @@ function generateStars(percentageFill) {
   const starFill = document.createElement('div');
   starFill.className = 'starFill';
   starFill.style.width = `${percentageFill}%`;
-  const starFillImage = document.createElement('img');
-  starFillImage.src = 'images/star.svg';
 
   const starOutline = document.createElement('div');
   starOutline.className = 'starOutline';
-  const starOutlineImage = document.createElement('img');
-  starOutlineImage.src = 'images/outline.svg';
 
-  newstarContainer.appendChild(starFill).appendChild(starFillImage);
-  newstarContainer.appendChild(starOutline).appendChild(starOutlineImage);
+  // newstarContainer.appendChild(starFill).appendChild(starFillImage);
+  newstarContainer.appendChild(starFill).appendChild(makeStar('filled'));
+  newstarContainer.appendChild(starOutline).appendChild(makeStar());
 
   document
     .querySelector('.holder')
